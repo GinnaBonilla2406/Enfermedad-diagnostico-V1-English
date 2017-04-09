1ef6c178df63edfda938e8dff4fa668dff7ae1d8
GinnaBonilla2406 <g2406b@gmail.com>
2017-04-08 20:13:04 -0500

    llamado-php.php

---
+++ llamado-php.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * Este php me permite usar el poder del php con un ejemplo de la tecnología AngularJS...
+     * incluso desde el administrador de este sitio.
+     */
+  
+include'BD.php';
+    $nuevo_obj=new BD();    // llama la clase BD
+           
+     if( isset( $_GET[ 'cadena' ] ) )
+    {     
+        $value=$_GET['cadena'];
+        echo  $nuevo_obj->consult($value);
+        //echo $sql;
+    }
+
+     if( isset( $_GET[ 'busqueda' ] ) )
+    {     
+        $value=$_GET['busqueda'];
+        echo  $nuevo_obj->search();
+        //echo $sql;
+    }
+?>
