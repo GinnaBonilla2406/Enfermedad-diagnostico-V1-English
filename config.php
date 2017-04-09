649cc8325cc6f94041730dc7e892b8d178b4efe3
GinnaBonilla2406 <g2406b@gmail.com>
2017-04-08 20:14:52 -0500

    config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+
+
+<?php 
+	$Server = "localhost";
+    $user = "root";
+    $key = "";
+    $bd = "bd_enfermedad";
+
+    //Puede colocar s o n minúsculas para ver diferentes impresiones de prueba de SQL y otras características.
+	 //$sn_diagnostico_clinico = "s";
+	 //$sn_escribir_log = "s";
+ ?>
